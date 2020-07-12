aa77ffde46363b65ab46df8419badc1e6a7558d8
evolveris <20345716+evolveris@users.noreply.github.com>
2020-07-12 20:05:26 +0100

    add stuff

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,18 @@ var express = require('express');
 var router = express.Router();
 const { uniqueNamesGenerator, adjectives, animals } = require('unique-names-generator');
 
-const shortRandomName = uniqueNamesGenerator({
-  dictionaries: [adjectives, animals], 
-  length: 2,
-  separator: '-',
-}); 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get("/nickname", (req, res) => {
-  return shortRandomName;
+  const shortRandomName = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals], 
+    length: 2,
+    separator: '-',
+  }); 
+  res.send(shortRandomName);
   // execute your function from server
 });
 
