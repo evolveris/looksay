d19d8bc4fa4ab8c47ee3f16e078c79b63b82f2b4
evolveris <20345716+evolveris@users.noreply.github.com>
2020-07-12 18:24:05 +0100

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
-
-const shortName = uniqueNamesGenerator({
-  dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
-  length: 2,
-  separator: '-',
-}); 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'looksay_', randomUsername: shortName, randomInitSequence: '112' });
+  res.render('index', { title: 'looksay_', randomInitSequence: '112' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const { uniqueNamesGenerator, adjectives, animals } = require('unique-names-generator');
+
+const shortRandomName = uniqueNamesGenerator({
+  dictionaries: [adjectives, animals], 
+  length: 2,
+  separator: '-',
+}); 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get("/nickname", (req, res) => {
+  return shortRandomName;
+  // execute your function from server
 });
 
 module.exports = router;
