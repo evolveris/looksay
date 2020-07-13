99e91046a82380354f565184bb449b2f0ae30a5c
evolveris <20345716+evolveris@users.noreply.github.com>
2020-07-13 19:59:10 +0100

    implemented chat in lobby

--- modules/socketio.js
+++ modules/socketio.js
@@ -8,16 +8,17 @@ module.exports = io => {
         console.log('a user connected');
       
         socket.on("player_registered", (playerInfo, fn) => { 
-
             const player = new Player(playerInfo.player_name);
             game.addPlayer(player);
-
             return fn(true);
         });
-    
+        
         socket.on("player_ready", (msg) => { console.log(msg) });
         socket.on("player_game_input", (msg) => { console.log(msg) });
-        socket.on("player_chat_message", (msg) => { console.log(msg) });
+        socket.on("player_chat_message", (msg, fn) => { 
+          console.log(msg) 
+          return fn(true);
+        });
     
     
         socket.on('disconnect', () => {
