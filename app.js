b69748ff82b99eadf3f1292a8ee14604fddade4d
evolveris <20345716+evolveris@users.noreply.github.com>
2020-08-11 14:37:14 +0100

    add endgame view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var playRouter = require('./routes/play');
+var endGameRouter = require('./routes/endgame');
 
 var app = express();
 app.use(cors());
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/play', playRouter);
+app.use('/endgame', endGameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/endgame.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const game = require("../modules/game");
+
+router.get('/', function(req, res, next) {
+    res.render('endgame', 
+    { 
+        user_score: '5', 
+        user_message: "You did grrreat!"
+        // TODO: add chart with all scores
+     });
+});
+
+module.exports = router;
